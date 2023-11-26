950587cba700ca2491db77d6111e3faafd0efb47
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-11-26 12:28:34 +0000

    Add basic structure for base.html and TradePostList view

--- tradeboard/views.py
+++ tradeboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import TradePost
+
 
 # Create your views here.
+class TradePostList(generic.ListView):
+    model = TradePost
+    queryset = TradePost.objects.filter(status=1).order_by('-created_at')
+    template_name = 'index.html'
+    paginate_by = 6
