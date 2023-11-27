888468898c9cbf6d1f8d1b77bfd17f7a8b1871f7
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-11-27 13:33:02 +0000

    Install crispy forms lib. Create forms.py file. Modify tradepost_detail.html for commenting. Add post method to TradePostDeatil views to enable commenting functionality

--- tradeboard/views.py
+++ tradeboard/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic, View
 from .models import TradePost
-
+from .forms import CommentForm
 
 # Create your views here.
 class TradePostList(generic.ListView):
@@ -26,8 +26,34 @@ class TradePostDetail(View):
             request,
             "tradepost_detail.html",
             {
-                "tradepost": tradepost, "comments": comments, "rated": rated
+                "tradepost": tradepost, "comments": comments,"commented": False, "rated": rated, "comment_form": CommentForm()
             },
         )
 
+    def post(self, request, slug, *args, **kwargs):
+        queryset = TradePost.objects.filter(status=1)
+        tradepost = get_object_or_404(queryset, slug=slug)
+        comments = tradepost.comments.filter(approved=True).order_by('created_at')
+        rated = False 
+        if tradepost.ratings.filter(id=self.request.user.id).exists():
+            rated = True
+
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment_form.instance.email = request.user.email
+            comment_form.instance.name = request.user.username
+            comment = comment_form.save(commit=False)
+            comment.tradepost = tradepost
+            comment.save()
+        else:
+            comment_form = CommentForm()
+
+
+        return render(
+            request,
+            "tradepost_detail.html",
+            {
+                "tradepost": tradepost, "comments": comments,"commented": True, "rated": rated, "comment_form": CommentForm()
+            },
+        )
 
