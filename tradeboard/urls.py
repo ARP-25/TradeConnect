b1ef820c0aec75061ff573340885c1be7f70518d
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-11-27 15:57:21 +0000

    Add functionality to rate a TradePost

--- tradeboard/urls.py
+++ tradeboard/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns = [
     path("", views.TradePostList.as_view(), name="home"),
     path("<slug:slug>/", views.TradePostDetail.as_view(), name="tradepost_detail"),
+    path('rating/<slug:slug>/', views.TradePostRating.as_view(), name='tradepost_rating'),
 ]
--- tradeboard/views.py
+++ tradeboard/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
-from .models import TradePost
+from django.http import HttpResponseRedirect
+from .models import TradePost, Rating
 from .forms import CommentForm
+
 
 # Create your views here.
 class TradePostList(generic.ListView):
@@ -18,15 +20,17 @@ class TradePostDetail(View):
         queryset = TradePost.objects.filter(status=1)
         tradepost = get_object_or_404(queryset, slug=slug)
         comments = tradepost.comments.filter(approved=True).order_by('created_at')
-        rated = False 
-        if tradepost.ratings.filter(id=self.request.user.id).exists():
-            rated = True
+
+        existing_rating = False
+        if request.user.is_authenticated:
+            existing_rating = Rating.objects.filter(post=tradepost, user=request.user).exists()
+
 
         return render(
             request,
             "tradepost_detail.html",
             {
-                "tradepost": tradepost, "comments": comments,"commented": False, "rated": rated, "comment_form": CommentForm()
+                "tradepost": tradepost, "comments": comments,"commented": False, "existing_rating": existing_rating, "comment_form": CommentForm()
             },
         )
 
@@ -34,9 +38,7 @@ class TradePostDetail(View):
         queryset = TradePost.objects.filter(status=1)
         tradepost = get_object_or_404(queryset, slug=slug)
         comments = tradepost.comments.filter(approved=True).order_by('created_at')
-        rated = False 
-        if tradepost.ratings.filter(id=self.request.user.id).exists():
-            rated = True
+
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
@@ -53,7 +55,29 @@ class TradePostDetail(View):
             request,
             "tradepost_detail.html",
             {
-                "tradepost": tradepost, "comments": comments,"commented": True, "rated": rated, "comment_form": CommentForm()
+                "tradepost": tradepost, "comments": comments,"commented": True, "comment_form": CommentForm()
             },
         )
+
+class TradePostRating(View):
+    def post(self, request, slug):
+        tradepost = get_object_or_404(TradePost, slug=slug)
+        existing_rating = Rating.objects.filter(post=tradepost, user=request.user).first()
+
+        if existing_rating:
+            return HttpResponseRedirect(reverse('tradepost_detail', args=[slug]))
+        
+        rating_value = request.POST.get('rating')
+
+        new_rating = Rating.objects.create(
+            post=tradepost,
+            user=request.user,
+            rating=rating_value
+        )
+
+        existing_rating = True
+
+        return HttpResponseRedirect(reverse('tradepost_detail', args=[slug]))
+
+
 
