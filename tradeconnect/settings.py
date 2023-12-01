e719fbc7b0bf8b5c7fc982b7becc39c98a9650d5
Angelo Rocco Pucci <angelo.pucci@outlook.de>
2023-12-01 17:25:50 +0000

    Testdeploy Commit

--- tradeconnect/settings.py
+++ tradeconnect/settings.py
@@ -23,7 +23,7 @@ EMAIL_HOST = 'smtp-mail.outlook.com'  # Outlook SMTP server
 EMAIL_PORT = 587  # Port for Outlook SMTP server (587 for TLS)
 EMAIL_USE_TLS = True  # Enable TLS (True for most SMTP servers)
 EMAIL_HOST_USER = 'angelo.pucci@outlook.de'  # Your Outlook email address
-EMAIL_HOST_PASSWORD = 'your_outlook_email_password'  # Your Outlook email password or app-specific password
+EMAIL_HOST_PASSWORD = 'angP2508+'  # Your Outlook email password or app-specific password
 DEFAULT_FROM_EMAIL = 'angelo.pucci@outlook.de'  # Sender's email address (usually same as EMAIL_HOST_USER)
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
